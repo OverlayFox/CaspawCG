26567bb7ac67fcfe6c3a8d2193242808cb7f3de6
OverlayFox <info@overlayfox.com>
2026-08-12 23:23:29 +0200

    added internal schedule logic

--- src/ui/update/schedule.go
+++ src/ui/update/schedule.go
@@ -3,6 +3,7 @@ package update
 import (
 	"context"
 	"errors"
+	"slices"
 	"time"
 
 	"github.com/overlayfox/caspaw-cg/src/types"
@@ -65,28 +66,38 @@ func NewSchedule(upstreamCtx context.Context, logger zerolog.Logger, template st
 
 func (s *Schedule) dropPastOffsets() {
 	now := time.Now()
+	removedOffsets := []int{}
 	keptOffsets := s.offsetSlice[:0:0]
 
-	for _, startOffset := range s.offsetSlice {
-		nextEventOffset := (startOffset + s.minEvents + 1) % len(s.offsetSlice)
+	for idx, startOffset := range s.offsetSlice {
+		nextEventIndex := (idx + s.minEvents + 1) % len(s.offsetSlice)
+		nextEventOffset := s.offsetSlice[nextEventIndex]
 
 		nextEventEnd, err := s.ScheduleSheet.EndTimeRange.GetRowData(nextEventOffset)
 		if err != nil {
 			s.logger.Error().Err(err).Msg("Failed to get next event end time")
 			continue
 		}
-		nextEventEndTime, ok := nextEventEnd.(time.Time)
+		nextEventEndStr, ok := nextEventEnd.(int64)
 		if !ok {
-			s.logger.Error().Err(errors.New("invalid type assertion")).Msg("Failed to assert next event end time")
+			s.logger.Error().Err(errors.New("invalid type assertion")).Msgf("Failed to assert next event end time as int, got %T", nextEventEnd)
 			continue
 		}
+		nextEventEndTime := time.Unix(nextEventEndStr, 0)
 
 		if now.After(nextEventEndTime) {
-			s.logger.Debug().Int("new_offset_slice_length", len(s.offsetSlice)).Msg("Removed expired event from offset slice")
+			s.logger.Debug().Int("new_offset_slice_length", len(s.offsetSlice)-len(removedOffsets)).Msg("Removed expired event from offset slice")
+			removedOffsets = append(removedOffsets, startOffset)
 			continue
 		}
-
 		keptOffsets = append(keptOffsets, startOffset)
+	}
+
+	// if the number of kept offsets is less than the minimum number of events, return the last few removed offsets to keep the minimum number
+	if len(keptOffsets) < s.minEvents {
+		amountToAdd := s.minEvents - len(keptOffsets)
+		slices.Sort(removedOffsets)
+		keptOffsets = append(removedOffsets[len(removedOffsets)-amountToAdd:], keptOffsets...)
 	}
 	s.offsetSlice = keptOffsets
 }
@@ -105,6 +116,33 @@ func resolveCasparData(casparMaps CasparMaps, offset int) (map[string]any, error
 }
 
 func (s *Schedule) Start() error {
+	s.logger.Debug().Interface("offset_slice", s.offsetSlice).Msg("Starting schedule update loop")
+	s.dropPastOffsets() // initial cleanup of expired events before starting the update loop
+	casparData := make(map[string]any)
+	s.currentOffset = s.offsetSlice[0]
+
+	for i := range s.minEvents {
+		casparMaps, err := s.ScheduleSheet.CasparMaps.GetBySlot(i)
+		if err != nil {
+			s.logger.Error().Err(err).Msg("Failed to get caspar maps by slot")
+			continue
+		}
+		desiredIndex := i % len(s.offsetSlice)
+		eventOffset := s.offsetSlice[desiredIndex]
+		casparEventData, err := resolveCasparData(casparMaps, eventOffset)
+		if err != nil {
+			s.logger.Error().Err(err).Msg("Failed to resolve caspar data")
+			continue
+		}
+		casparData = mergeMaps(casparData, casparEventData)
+	}
+
+	s.logger.Debug().Interface("casparData", casparData).Msg("Initial schedule update")
+	if err := s.casparCGClient.AddCGData(s.template, s.layer, s.videoChannels, casparData, s.sizing, s.delay); err != nil {
+		s.logger.Error().Err(err).Msg("Failed to push initial CG data")
+		return err
+	}
+
 	s.wg.Go(func() {
 		for {
 			select {
@@ -112,28 +150,26 @@ func (s *Schedule) Start() error {
 				return
 			case <-time.After(s.updateInterval):
 				if len(s.offsetSlice) <= s.minEvents {
+					s.logger.Info().Str("template", s.template).Int("layer", s.layer).Interface("video_channels", s.videoChannels).Msg("Stopping schedule update loop: no more events to display")
 					return // terminate the update loop
 				}
 				s.dropPastOffsets()
 
 				casparData := make(map[string]any)
-				for _, startOffset := range s.offsetSlice {
+				for idx, startOffset := range s.offsetSlice {
 					if startOffset == s.currentOffset {
-						desiredOffset := (startOffset + 1) % len(s.offsetSlice) // move forward by one
-						offset := s.offsetSlice[desiredOffset]                  // get the actual offset
+						nextIndex := (idx + 1) % len(s.offsetSlice)
+						offset := s.offsetSlice[nextIndex] // get the actual next offset value
+						s.currentOffset = offset
 						for i := range s.minEvents {
-							if i == 0 {
-								s.currentOffset = offset
-							}
-
 							casparMaps, err := s.ScheduleSheet.CasparMaps.GetBySlot(i)
 							if err != nil {
 								s.logger.Error().Err(err).Msg("Failed to get caspar maps by slot")
 								continue
 							}
 
-							desiredOffset := (offset + i) % len(s.offsetSlice)
-							eventOffset := s.offsetSlice[desiredOffset]
+							desiredIndex := (nextIndex + i) % len(s.offsetSlice)
+							eventOffset := s.offsetSlice[desiredIndex]
 							casparEventData, err := resolveCasparData(casparMaps, eventOffset)
 							if err != nil {
 								s.logger.Error().Err(err).Msg("Failed to resolve caspar data")
@@ -141,6 +177,7 @@ func (s *Schedule) Start() error {
 							}
 							casparData = mergeMaps(casparData, casparEventData)
 						}
+						break // only process the first matching offset to avoid duplicate updates
 					}
 				}
 
